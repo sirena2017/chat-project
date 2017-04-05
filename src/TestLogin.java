3746faac8e8187418a6079f959c9b4559a3b853f
namak <mazhari44@gmail.com>
2017-04-05 17:13:16 +0430

    testLogin created

---
+++ src/TestLogin.java
@@ -0,0 +1,97 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+/**
+ * Servlet implementation class testLogin
+ */
+@WebServlet("/testLogin")
+public class TestLogin extends HttpServlet {
+	
+	EntityManagment em;
+	User user;
+	static ArrayList<String> listOfOnline;
+	static ArrayList<HttpSession> sessions;
+	
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestLogin() {
+        super();
+        em=new EntityManagment();
+        user=new User();
+        listOfOnline=new ArrayList<String>();
+        sessions=new ArrayList<HttpSession>();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username=request.getParameter("user");
+		user.setUsername(username);
+		String pass=request.getParameter("password");
+		user.setPassword(pass);
+		String result=em.userExist(user);
+		
+		String referer = request.getHeader("Referer");
+		//request.getSession().setAttribute("message", result);
+		
+		if(result.equals("USER TRUE"))
+		{
+			
+			HttpSession s=request.getSession();
+			s.setAttribute("isLogedin", true);	
+			s.setAttribute("id", username);	
+			//s.setAttribute("username", user.getUsername());
+			s.setAttribute("password", pass);
+			sessions.add(s);
+			listOfOnline.add(username);
+			
+			int n=referer.length();
+			int i=n-1;
+			while(referer.charAt(i)!='/')
+			{
+				i--;
+			}
+			
+			referer=referer.substring(0, i)+"/InfoPage";
+			response.getWriter().append(referer+"referer is::");
+			response.sendRedirect(referer);
+			
+		}
+		else 
+		{
+			request.getSession().setAttribute("isLogedin", false);
+			response.sendRedirect(referer);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
