1b44c87e89e1bf9efcd28e71edab1ab80bd3b6e2
Massy <sirena1369@yahoo.com>
2017-04-14 15:14:25 +0430

    textarea changed to contenteditable div

--- src/CreatChatConnection.java
+++ src/CreatChatConnection.java
@@ -74,11 +74,11 @@ public class CreatChatConnection extends HttpServlet {
 				+ "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
 				+ "<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'></head>"
 				+ "<body><br><<br><div class='container'><div class='col-md-2 col-md-offset-3'><form action='/ChatProject/MessageHandling' method='GET'>"
-				+ "<p>" + starter +groupNames
-				+ "</p><textarea name='history' readonly='true' class='form-control custom-control'  style='width:500px; height: 400px;'>"
+				+ "<p>" + starter +groupNames+"</p>"
+				+ "<div class='input-group'>"
+				+ "<div name='history' id='history' class='form-control custom-control' style='width:500px; height: 300px;'></div>"
 				+ message + "</textarea>"
-				+ "<br><div class='input-group'><textarea name='message' class='form-control custom-control' rows='2' placeholder='Write Message Here' style='width:500px;resize:none'>"
-				+ "</textarea><button type='submit' class='btn btn-info'>Send</button> "
+				+ "<br><div class='input-group'><div name='message' id='message' contenteditable='true' class='form-control custom-control' rows='2' placeholder='Write Message Here' style='width:280px;resize:none'></div><button type='submit' class='btn btn-info'>Send</button> "
 				+ "</div></form>"
 				+ "<form action='/ChatProject/LogOut' method='GET'><button type='submit' class='btn btn-info'>Logout</button></form>"
 				+"</div></div></body></html>";
