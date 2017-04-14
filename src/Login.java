ff6b2723adedeaa2476ac9c0dcccb6fa5326dd6a
Massy <sirena1369@yahoo.com>
2017-04-15 02:58:46 +0430

    template added to login page

--- src/Login.java
+++ src/Login.java
@@ -29,13 +29,23 @@ public class Login extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		String s = "<html><head>"
-				+ "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
-				+ "</head><body background='rNjDs2.jpg'><div class='container'> "
-				+ " <div class='col-md-3 col-md-offset-4'>      "
-				+ "<form action='/ChatProject/TestLogin' method='POST'>    <br><br>    <div class='panel panel-info'> <div class='panel-heading'> <h3 class='panel-title'>Login</h3> </div>      <div class='panel-body'>	 	  <div class='input-group'>   "
-				+ "    <input type='text' name='user' class='form-control' placeholder='Username' aria-describedby='basic-addon1'>      </div> "
-				+ "    <br>     <div class='input-group'>      <input type='password' name='password' class='form-control' placeholder='Password' aria-describedby='basic-addon1'>     </div>      <br>     <button type='submit' class='btn btn-info'>Sign in</button>     </div>    </div></form>	</div></div></body></html>";
+		String s = "<html lang='en'><head><title>List Page</title>"
+				  +"<meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'>"
+				  +"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"
+				  +"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"
+				    +"<style> .row.content {height: 1500px}"
+				    +".sidenav {background-color: #f1f1f1;height: 100%; }"
+				    +"footer {background-color: #555;color: white;padding: 15px;}" 
+				    +"@media screen and (max-width: 767px) {.sidenav {height: auto;padding: 15px;}"
+				      +".row.content {height: auto;} }</style></head>"
+				    
+				+ "<body><div class='container-fluid'>  <div class='row content'>    <div class='col-sm-3 sidenav'>      <img src='http://prateekjain.co/resources/Chat%20Client%20Logo.png'></img>"
+      +"<ul class='nav nav-pills nav-stacked'>        <li ><a>Welcome back!!</a></li>        <li><a>You can have live chat here</a></li>      </ul><br>      </div>"
+    +"<div class='col-sm-9'>	  <br>   <form action='/ChatProject/TestLogin' method='POST'>    <br><br>	<div class='col-md-4 col-md-offset-3'>"
+    +"<div class='panel panel-info'> <div class='panel-heading'> <h3 class='panel-title'>Please Sign In</h3> </div>      <div class='panel-body'>	 <div class='input-group'>"
+       +"<input type='text' name='user' id='user' class='form-control' placeholder='Username' aria-describedby='basic-addon1'>      </div>     <br>     <div class='input-group'>"
+      +"<input type='text' name='password' class='form-control' placeholder='Password' aria-describedby='basic-addon1'></div> <br><button type='submit' class='btn btn-info'>Sign in</button></div></div></div></form></div></div></div>"
+		+"<footer class='container-fluid'><p>contact us</p><p>Mazhari44@gmail.com</p><p>Ghelichkhani.m@gmail.com</p></footer></body></html>";
 
 		response.getWriter().append(s);
 
